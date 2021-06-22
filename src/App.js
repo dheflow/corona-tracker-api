dec440d0a5c4fd91c6f426643d134e12f9662514
Tri Hariyanto <dheflow071@gmail.com>
2021-06-22 10:56:34 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData} from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+  
+  render () {
+    return (
+      <div className={styles.container}>
+        <h1>Covid track App</h1>
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    )
+  }
 }
 
 export default App;

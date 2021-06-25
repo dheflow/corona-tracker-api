a66a36118cd244c4083bf6c4d7e4328b4ca27610
Tri Hariyanto <dheflow071@gmail.com>
2021-06-25 19:44:19 +0700

    project finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { Cards, Chart, CountryPicker } from './components';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
 import styles from './App.module.css';
-import { fetchData} from './api';
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
   async componentDidMount() {
     const data = await fetchData();
 
-    console.log(data);
+    this.setState({ data });
   }
-  
-  render () {
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
     return (
       <div className={styles.container}>
-        <h1>Covid track App</h1>
-        <Cards />
-        <Chart />
-        <CountryPicker />
+        <h1>Covid Tracker App</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+        <h5>Created By Tri Hariyanto</h5>
       </div>
-    )
+    );
   }
 }
 
